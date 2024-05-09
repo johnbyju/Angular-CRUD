48d2d3a56044678b0e52289943674040400305da
johnbyju <johnbyju8@gmail.com>
2024-05-10 00:00:25 +0530

    auth-m

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,90 +1,114 @@
 import { CommonModule } from '@angular/common';
-import { Component, viewChild} from '@angular/core';
-import { FormsModule,} from '@angular/forms';
+import { Component, viewChild } from '@angular/core';
+import { FormsModule, } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,FormsModule,],
+  imports: [CommonModule, FormsModule,],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  sigindiv:boolean= true;
-  signUpDetails :signup = new signup();
-  loginDetails : login =new login();
+  sigindiv: boolean = true;
+  signUpDetails: signup = new signup();
+  loginDetails: login = new login();
 
- //---------authunticate---------
- 
- constructor(){}
+  //---------authunticate---------
 
- loginFunction(){
-  if(this.isloginValid()){
-    
-  }
+  constructor() { }
 
+  loginFunction() {
+    if (this.isloginValid()) {
+      const getUser = localStorage.getItem('users');
+      if (getUser !== null) {
+        const checkUser = JSON.parse(getUser)
+        const matchUser = checkUser.find((user: signup) => {
+          return user.email.trim().toLocaleLowerCase() === this.loginDetails.email.trim().toLocaleLowerCase() &&
+            user.password.trim().toLocaleLowerCase() === this.loginDetails.password.trim().toLocaleLowerCase()
+        });
 
- }
- register() {
-  if (this.isSignUpValid()) {
-    if (this.signUpDetails.name.trim().toLocaleLowerCase() === '') {
-      alert('Name field is empty');
-    } else if (this.signUpDetails.email.trim().toLocaleLowerCase() === '') {
-      alert('Email field is empty');
-    } else if (this.signUpDetails.password.trim().toLocaleLowerCase() === '') {
-      alert('Password field is empty');
-    } else {
-      const users = localStorage.getItem('users');
-      if (users !== null) {
-        const userArray = JSON.parse(users);
-        userArray.push(this.signUpDetails);
-        localStorage.setItem('users', JSON.stringify(userArray));
-      } else {
-        const userArray = [this.signUpDetails];
-        localStorage.setItem('users', JSON.stringify(userArray));
+        if (matchUser) {
+          alert('login succesfully ') // and move the user into the site accessble
+        }
+        else {
+          alert('invalid user details')
+        }
       }
+      else {
+        alert('no user registerd it ')
+      }
+      this.loginDetails = new login();
     }
-  } else {
-    console.log('Invalid signup details');
+    else {
+      alert('enter the email and password correctly')
+    }
+
+
+  }
+  register() {
+    if (this.isSignUpValid()) {
+      if (this.signUpDetails.name.trim().toLocaleLowerCase() === '') {
+        alert('Name field is empty');
+      } else if (this.signUpDetails.email.trim().toLocaleLowerCase() === '') {
+        alert('Email field is empty');
+      } else if (this.signUpDetails.password.trim().toLocaleLowerCase() === '') {
+        alert('Password field is empty');
+      } else {
+        const users = localStorage.getItem('users');
+        if (users !== null) {
+          const userArray = JSON.parse(users);
+          userArray.push(this.signUpDetails);
+          localStorage.setItem('users', JSON.stringify(userArray));
+        } else {
+          const userArray = [this.signUpDetails];
+          localStorage.setItem('users', JSON.stringify(userArray));
+        }
+      }
+      this.signUpDetails = new signup();
+    } 
+     else {
+      alert('Kindly Enter your details');
+    }
+   
+
+
+  }
+  isSignUpValid(): boolean {
+    return (
+      this.signUpDetails.name.trim().toLocaleLowerCase() !== '' &&
+      this.signUpDetails.email.trim().toLocaleLowerCase() !== '' &&
+      this.signUpDetails.password.trim().toLocaleLowerCase() !== ''
+    );
+  }
+  isloginValid(): boolean {
+    return (
+      this.loginDetails.email.trim().toLocaleLowerCase() !== '' &&
+      this.loginDetails.password.trim().toLocaleLowerCase() !== ''
+    );
   }
 
-  
-}
-isSignUpValid(): boolean  {
-  return(
-    this.signUpDetails.name.trim().toLocaleLowerCase() !== '' &&
-    this.signUpDetails.email.trim().toLocaleLowerCase() !== '' &&
-    this.signUpDetails.password.trim().toLocaleLowerCase() !== ''
-  );
-}
-isloginValid () :boolean {
-  return(
-    this.loginDetails.email.trim().toLocaleLowerCase() !== ''&& 
-    this.loginDetails.password.trim().toLocaleLowerCase() !== '' 
-  );
-}
-  
 }
 
-export class signup  {
-     
-  name : string ;
-  email : string ;
-  password :string ;
+export class signup {
 
-  constructor (){
-    this.name ="";
-    this.email="";
-    this.password ="";
+  name: string;
+  email: string;
+  password: string;
+
+  constructor() {
+    this.name = "";
+    this.email = "";
+    this.password = "";
 
   }
 }
 
 export class login {
-  email :string ;
-  password : string ;
-  constructor(){
-    this.email ="";
-    this.password ="";
+  email: string;
+  password: string;
+  constructor() {
+    this.email = "";
+    this.password = "";
   }
 }
