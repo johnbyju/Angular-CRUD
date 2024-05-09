e6f4c8bad6e5840d8d5389f95886cc31eda41f83
johnbyju <johnbyju8@gmail.com>
2024-05-09 16:59:43 +0530

    valdating over

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component} from '@angular/core';
+import { Component, viewChild} from '@angular/core';
 import { FormsModule,} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule,FormsModule,],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -13,28 +13,42 @@ export class LoginComponent {
   sigindiv:boolean= true;
   signUpDetails :signup = new signup();
   loginDetails : login =new login();
+//---------authunticate---------
+ 
+ constructor(){}
 
-  loginFormSubmit(){
-  
-  }
-  signupFormSubmit(){
-
-}
-
-  register(){
-     const users = localStorage.getItem('users');
-    if (users != null){
-      const userArray = JSON.parse(users);
-      userArray.push(this.signUpDetails);
-      localStorage.setItem('users',JSON.stringify(userArray));
-    }else{
-      const userArray = [];
-      userArray.push(this.signUpDetails);
-      localStorage.setItem('users',JSON.stringify(userArray));
-
+ register() {
+  if (this.isSignUpValid()) {
+    if (this.signUpDetails.name.trim().toLocaleLowerCase() === '') {
+      alert('Name field is empty');
+    } else if (this.signUpDetails.email.trim().toLocaleLowerCase() === '') {
+      alert('Email field is empty');
+    } else if (this.signUpDetails.password.trim().toLocaleLowerCase() === '') {
+      alert('Password field is empty');
+    } else {
+      const users = localStorage.getItem('users');
+      if (users !== null) {
+        const userArray = JSON.parse(users);
+        userArray.push(this.signUpDetails);
+        localStorage.setItem('users', JSON.stringify(userArray));
+      } else {
+        const userArray = [this.signUpDetails];
+        localStorage.setItem('users', JSON.stringify(userArray));
+      }
     }
-
+  } else {
+    console.log('Invalid signup details');
   }
+  
+}
+isSignUpValid(): boolean  {
+  return(
+    true
+    // this.signUpDetails.name.trim().toLocaleLowerCase() !== '' &&
+    // this.signUpDetails.email.trim().toLocaleLowerCase() !== '' &&
+    // this.signUpDetails.password.trim().toLocaleLowerCase() !== ''
+  );
+}
   
 }
 
